8c2e5c016624261fab8c084543523f1ca3eeedd1
FranciscoPagoaga <francisco.pagoaga14@gmail.com>
2020-08-23 20:27:35 -0600

    Updates

---
+++ precache-manifest.6a0ebbc6837fea94ab1ba383a3b17f8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e269d1df45507bdda82178604e568319",
+    "url": "/Examen1_UX/index.html"
+  },
+  {
+    "revision": "d1635121e0ae2c6e828d",
+    "url": "/Examen1_UX/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "bb4419f9bb1d68c0c4f2",
+    "url": "/Examen1_UX/static/css/main.45b0aadb.chunk.css"
+  },
+  {
+    "revision": "d1635121e0ae2c6e828d",
+    "url": "/Examen1_UX/static/js/2.fa21f376.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Examen1_UX/static/js/2.fa21f376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4419f9bb1d68c0c4f2",
+    "url": "/Examen1_UX/static/js/main.4f0ed1b8.chunk.js"
+  },
+  {
+    "revision": "d2e00c75da550282e9c8",
+    "url": "/Examen1_UX/static/js/runtime-main.d0fec350.js"
+  }
+]);
